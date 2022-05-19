feef59decaea3ab2bb074cb5cb7608b1650f2d49
Wellington Ferreira <wellingtonf.souza@outlook.com>
2022-05-18 22:44:25 -0300

    feat: adiciona o memrepo (repositorio)

---
+++ rentomatic/repository/memrepo.py
@@ -0,0 +1,8 @@
+from rentomatic.domain.room import Room
+
+class MemRepo:
+    def __init__(self,data) -> None:
+        self.data = data
+
+    def list(self):
+        return [Room.from_dict(i) for i in self.data]
